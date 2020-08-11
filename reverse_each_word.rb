6f54f3a7cd8fcd35e8e4143d34f387026aa03f2b
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-11 16:13:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,8 @@ def reverse_each_word(str)
   str_into_array = str.split()
   str_into_array.each do { |el| el.reverse! }
   
-  new_reversed_str = str_into_array.join
+  new_reversed_str = str_into_array.join(" ")
+  
+  return new_reversed_str
   
 end
